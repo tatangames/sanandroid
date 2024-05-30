c45ca496c2e2874edf718a90b9569611eb8142c6
tatangames <54828743+tatangames@users.noreply.github.com>
2024-05-30 16:02:29 -0600

    pantalla login y logo

--- app/src/main/java/com/alcaldiasan/santaananorteapp/fragmentos/login/FragmentLogin.java
+++ app/src/main/java/com/alcaldiasan/santaananorteapp/fragmentos/login/FragmentLogin.java
@@ -1,9 +1,12 @@
 package com.alcaldiasan.santaananorteapp.fragmentos.login;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,11 +15,55 @@ import com.alcaldiasan.santaananorteapp.R;
 public class FragmentLogin extends Fragment {
 
 
+    private EditText edtTelefono;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_login, container, false);
 
+        edtTelefono = vista.findViewById(R.id.edtTelefono);
 
+        edtTelefono.addTextChangedListener(new TextWatcher() {
+            private String current = "";
+            private String previous = "";
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                previous = s.toString();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    edtTelefono.removeTextChangedListener(this);
+
+                    String cleanString = s.toString().replaceAll("[^\\d]", "");
+                    String formattedString = "";
+
+                    if (cleanString.length() > 4) {
+                        formattedString = cleanString.substring(0, 4) + " " + cleanString.substring(4);
+                    } else {
+                        formattedString = cleanString;
+                    }
+
+                    if (formattedString.length() > 9) {
+                        formattedString = formattedString.substring(0, 9);
+                    }
+
+                    current = formattedString;
+                    edtTelefono.setText(formattedString);
+                    edtTelefono.setSelection(formattedString.length());
+
+                    edtTelefono.addTextChangedListener(this);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // No action needed here
+            }
+        });
 
 
         return vista;
